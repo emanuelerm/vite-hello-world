6fb9c0e91a76cd7bc5ace99265f2b0d29583d35c
emanuelerm <emanueler00@gmail.com>
2023-04-12 15:18:13 +0200

    vite-hello-world

---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <div>
+      <img src="../assets/vite-jumbo.webp" alt="vite-jumbo" />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "AppHero",
+};
+</script>
+
+<style lang="scss" scoped></style>
